9806e11e9a5c570caccf4d59130bf590a9c5286a
prade <pradeeppatwal06@gmail.com>
2022-05-10 22:38:20 +0530

    first commit

---
+++ src/main/java/com/pradeep/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.springjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
